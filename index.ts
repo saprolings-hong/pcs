683887c492260068196877e39e254a2d3a1db0da
saprolings-hong <hong@saprolings.com>
2025-12-17 22:13:56 +0800

    permit

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import { ethers } from "ethers";
-import { formatUnits, parseUnits } from "ethers/lib/utils";
 import type { PancakeSwapOptions } from "@pancakeswap/universal-router-sdk";
-import { PancakeSwapUniversalRouter } from "@pancakeswap/universal-router-sdk";
+import {
+  PancakeSwapUniversalRouter,
+  getUniversalRouterAddress,
+  type Permit2Signature,
+} from "@pancakeswap/universal-router-sdk";
+import { MaxAllowanceTransferAmount, PermitSingle, getPermit2Address, PERMIT_EXPIRATION, PERMIT_SIG_EXPIRATION, AllowanceTransfer, Permit2ABI } from "@pancakeswap/permit2-sdk";
 import { Percent, Native, ChainId, CurrencyAmount, TradeType, ERC20Token } from "@pancakeswap/sdk";
 import {
   InfinityRouter,
@@ -13,23 +17,17 @@ import {
   InfinityBinPool,
   InfinityClPool,
   type Route,
+  type OnChainProvider,
 } from "@pancakeswap/smart-router";
 import { bscTokens } from "@pancakeswap/tokens";
-import {
-  createPublicClient,
-  http,
-  decodeAbiParameters,
-  decodeFunctionData,
-} from "viem";
+import { createPublicClient, http, decodeFunctionData, type Address } from "viem";
 import { bsc } from "viem/chains";
 import { findBestTrade, type Pool, type TradeWithGraph } from "@pancakeswap/routing-sdk";
 import { createV2Pool } from "@pancakeswap/routing-sdk-addon-v2";
 import { createV3Pool } from "@pancakeswap/routing-sdk-addon-v3";
-import {
-  createInfinityBinPool,
-  createInfinityCLPool
-} from "@pancakeswap/routing-sdk-addon-infinity";
+import { createInfinityBinPool, createInfinityCLPool } from "@pancakeswap/routing-sdk-addon-infinity";
 import { createStablePool } from "@pancakeswap/routing-sdk-addon-stable-swap";
+import { decodeUniversalCalldata } from "./utils/calldataDecode";
 
 const getArgs = () => {
   const flags = ["--pk", "--amount", "--fromToken", "--toToken"];
@@ -119,31 +117,46 @@ export const buildInfinityTrade = (
   };
 };
 
+const toDeadline = (expiration: number): number => {
+  return Math.floor((Date.now() + expiration) / 1000)
+}
+
+export const makePermit = (
+  token: Address,
+  // as spender
+  routerAddress: Address,
+  amount: string = MaxAllowanceTransferAmount.toString(),
+  nonce = 0
+): PermitSingle => {
+  return {
+    details: {
+      token,
+      amount,
+      expiration: toDeadline(PERMIT_EXPIRATION).toString(),
+      nonce,
+    },
+    spender: routerAddress,
+    sigDeadline: toDeadline(PERMIT_SIG_EXPIRATION).toString(),
+  };
+};
+
 const pancakeRouterAbi = require("./abis/pancakeSwapRouter.json");
 const bep20Abi = require("./abis/bep20.json");
 
 // mainnet
 const pancakeRouterAddressInBsc = "0xd9c500dff816a1da21a48a732d3498bf09dc9aeb";
-const cakeTokenAddressInBsc = "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82";
 const bscRpcUrl = "https://bsc-dataseed.binance.org/";
 
 const executeTransaction = async () => {
   const [privateKey, amount, fromToken, toToken] = getArgs();
   const swapFrom = getTokenBySymbol(fromToken);
   const swapTo = getTokenBySymbol(toToken);
-  const amountInput = parseUnits(amount, 18);
+  const amountInput = ethers.parseUnits(amount, 18);
 
   console.log(`Swap ${amount} ${fromToken} to ${toToken}`);
 
-  const provider = new ethers.providers.JsonRpcProvider(bscRpcUrl);
+  const provider = new ethers.JsonRpcProvider(bscRpcUrl);
   const wallet = new ethers.Wallet(privateKey, provider);
-  // const bnbBalance = await provider.getBalance(wallet.address);
-  // console.log(`BNB Balance: ${formatUnits(bnbBalance, 18)}`);
-  // const signer = provider.getSigner(wallet.address);
-  // const cakeTokenContract = new ethers.Contract(cakeTokenAddressInBsc, bep20Abi, provider);
-  // const cakeBalance = await cakeTokenContract.balanceOf(wallet.address);
-  // console.log(`CAKE Balance: ${formatUnits(cakeBalance, 18)}`);
-
   const client = createPublicClient({
     chain: bsc,
     transport: http("https://bsc-dataseed.binance.org"),
@@ -153,14 +166,13 @@ const executeTransaction = async () => {
       },
     },
   });
-  // const chainId = ChainId.BSC;
-  // const swapFrom = Native.onChain(chainId);
-  // const swapTo = bscTokens.cake;
-  const pools = await InfinityRouter.getInfinityCandidatePools({
-    clientProvider: () => client,
+
+  const pools = await InfinityRouter.getInfinityClCandidatePools({
+    clientProvider: (() => client) as OnChainProvider,
     currencyA: swapFrom,
     currencyB: swapTo,
   });
+  // const filteredPools = pools.filter((pool) => pool.type === PoolType.InfinityCL);
   // const v3Pools = await InfinityRouter.getV3CandidatePools({
   //   clientProvider: () => client,
   //   currencyA: swapFrom,
@@ -174,7 +186,7 @@ const executeTransaction = async () => {
   //   throw new Error("Unable to find a valid trade route");
   // }
 
-  const amountIn = CurrencyAmount.fromRawAmount(swapFrom, amountInput.toBigInt());
+  const amountIn = CurrencyAmount.fromRawAmount(swapFrom, amountInput);
   const bestTrade = await findBestTrade({
     amount: amountIn,
     quoteCurrency: swapTo,
@@ -187,28 +199,64 @@ const executeTransaction = async () => {
     throw new Error("Unable to find a valid trade route");
   }
 
-  const options = swapOptions({});
+  let permit2Permit: Permit2Signature | undefined;
+  if (swapFrom instanceof ERC20Token) {
+    const PERMIT2_ADDRESS = getPermit2Address(ChainId.BSC);
+    const UNIVERSAL_ROUTER_ADDRESS = getUniversalRouterAddress(ChainId.BSC);
+    const permit2Contract = new ethers.Contract(PERMIT2_ADDRESS!, Permit2ABI, wallet);
+    const [ permit2AllowanceAmount, permit2AllowanceExpiration, permit2AllowanceNonce ] = await permit2Contract.allowance(wallet.address, swapFrom.address, UNIVERSAL_ROUTER_ADDRESS);
+
+    const permitSingle = makePermit(
+      swapFrom!.address,
+      UNIVERSAL_ROUTER_ADDRESS,
+      amountInput.toString(),
+      permit2AllowanceNonce,
+    );
+    
+    const { domain, types, values } = AllowanceTransfer.getPermitData(permitSingle, PERMIT2_ADDRESS, ChainId.BSC)
+    const signature = await wallet.signTypedData(domain, types, values) as `0x${string}`;
+
+    const erc20Contract = new ethers.Contract(swapFrom.address, bep20Abi, wallet);
+    const ecr20Allowance = await erc20Contract.allowance(wallet.address, PERMIT2_ADDRESS);
+     // Default ERC20 approve amount 500, reduce the number of approve calls
+    const DEFAULT_APPROVE_AMOUNT = ethers.parseUnits("500", swapFrom.decimals);
+    const approveAmount = amountInput > DEFAULT_APPROVE_AMOUNT ? amountInput : DEFAULT_APPROVE_AMOUNT;
+    if (ecr20Allowance < amountInput) {
+      console.log(`Current ${swapFrom.symbol} allowance ${ethers.formatUnits(ecr20Allowance, swapFrom.decimals)} is less than amount to swap ${ethers.formatUnits(amountInput, swapFrom.decimals)}`);
+      const tx = await erc20Contract.approve(PERMIT2_ADDRESS, approveAmount);
+      await tx.wait();
+      console.log(`${swapFrom.symbol} approve Tx hash:`, tx.hash);
+    }
+
+    permit2Permit = {
+      ...permitSingle,
+      signature,
+    };
+  }
+
+  const options = permit2Permit ? swapOptions({ inputTokenPermit: permit2Permit }) : swapOptions({});
   const smartRouterTrade = buildInfinityTrade(TradeType.EXACT_INPUT, bestTrade);
   const { calldata, value } = PancakeSwapUniversalRouter.swapERC20CallParameters(smartRouterTrade, options);
-  const { functionName, args } = decodeFunctionData({
-    abi: pancakeRouterAbi,
-    data: calldata,
-  });
 
-  // console.log("Calldata:", calldata)
-  // console.log("Value:", value)
+  // const { functionName, args } = decodeFunctionData({
+  //   abi: pancakeRouterAbi,
+  //   data: calldata,
+  // });
+  // console.log("Calldata:", calldata);
+  // console.log("Value:", value);
   // console.log("Function Name:", functionName);
   // console.log("Args:", args);
+  // const decodedCommands = decodeUniversalCalldata(calldata);
 
   const tx = await wallet.sendTransaction({
     to: pancakeRouterAddressInBsc,
     data: calldata,
     value: value,
-    gasLimit: ethers.BigNumber.from(700000),
+    gasLimit: 700000n,
   });
 
-  const receipt = await tx.wait();
-  console.log("Transaction:", receipt);
+  const transaction = await tx.wait();
+  console.log("Transaction:", transaction);
 };
 
 (async () => {
